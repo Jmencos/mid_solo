b2c11024deb067e5aca5851a77c58331ea52a967
Jose Mencos <j.mencos56@gmail.com>
2022-12-20 09:15:41 -0500

    tuesday morning

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -12,19 +12,20 @@ function LoginForm({onFormSwitch}) {
 
 
   return (
-    <>
+    <div className ="auth-form">
+      <h2> Login</h2>
       <form className="login" onSubmit={handleSubmit}>
-        <label htmlFor="email">Email</label>
+        <label htmlFor="email">Enter your email </label>
         <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="Enter your email" id="email" name="email"></input>
         <br></br>
-        <label htmlFor="password">Enter your password</label>
+        <label htmlFor="password">Enter password </label>
         <input value={pass} onChange={(e)=> setPass(e.target.value)} type="password" placeholder="******" id="passsword" name="password"></input>
         <br></br>
         <button type="submit">Log in</button>
       </form>
       <br></br>
-      <button onClick={()=> onFormSwitch('register')}>Register here</button>
-    </>
+      <button className="link-btn" onClick={()=> onFormSwitch('register')}>Need an account? Register here</button>
+    </div>
   )
 }
 
--- src/components/register.jsx
+++ src/components/register.jsx
@@ -6,20 +6,21 @@ function Register({onFormSwitch}) {
   const [name, setName] = useState('');
 
   return (
-    <>
-      <form>
-        <label htmlFor="name">Full Name</label>
+    <div className="auth-form">
+      <h2>Register</h2>
+      <form className="register">
+        <label htmlFor="name">Full Name </label>
         <input onChange={(e) => setName(e.target.value)} value={name} name="name" id="name" placeholder="Full name here" ></input>
         <br></br>
-        <label htmlFor="email">Email</label>
+        <label htmlFor="email">Email </label>
         <input onChange={(e) => setEmail(e.target.value)} value={email} name="email" id="email" placeholder="JohnDoe@email.com" type="email"></input>
         <br></br>
-        <label htmlFor="password">Enter a password</label>
+        <label htmlFor="password">Enter password </label>
         <input onChange={(e) => setPass(e.target.value)} value={pass} type="password" placeholder="*****" id="password"></input>
       </form>
       <br></br>
-      <button onClick={()=>onFormSwitch('login')}>Have an account? Click here </button>
-    </>
+      <button className="link-btn" onClick={()=>onFormSwitch('login')}>Have an account? Click here </button>
+    </div>
   )
 }
 
