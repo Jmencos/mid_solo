c9eb5338b4d28bdba0f67273acccc642472820a7
Chris K <Chris-SourceCode@outlook.com>
2022-12-22 01:32:19 +0100

    build: organize prodcution config

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,13 +4,15 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const common = require('./webpack.common.js');
 
 const production = merge(common, {
+  mode: 'production',
+
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'build'),
     publicPath: '/',
     clean: true,
   },
-  mode: 'production',
+
   optimization: {
     minimize: true,
     minimizer: ['...', new MiniCssExtractPlugin()], // "..." used to include default minimizers
